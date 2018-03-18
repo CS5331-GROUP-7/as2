187d0152d799535275d0e87580a2490c9fe28d9b
Zaw Lin <you@example.com>
2018-03-18 22:29:37 +0800

    * roll back c26 and add c06

---
+++ c06/case06.php
@@ -0,0 +1,10 @@
+<html>
+	<h1>Case 06</h1>
+		
+	
+	<body>
+		Hello World! <br/> <br/>
+		<script src="https://www.wsb.com/Assignment2/global-libs/jquery-2.1.3.js"></script>
+		<img src="http://www.wsb.com/Assignment2/res/images/image.png"/>
+	</body>
+</html>
---
+++ c06/load_page.py
@@ -0,0 +1,4 @@
+import webbrowser
+
+url = "http://www.wsb.com/Assignment2/case06.php"
+webbrowser.open_new_tab(url)
